7bac24756d015195539771cd06d9d9cf03516fd4
tan <tanmaykm@gmail.com>
2014-03-02 00:28:02 +0530

    requires Julia 0.3, primarily because of Dict constructor.

--- src/ProtoBuf.jl
+++ src/ProtoBuf.jl
@@ -4,19 +4,6 @@ import Base.show
 
 export writeproto, readproto, ProtoMeta, ProtoMetaAttribs, meta, show, filled, fillset, fillunset
 
-# Julia 0.2 compatibility patch
-if isless(Base.VERSION, v"0.3.0-")
-import Base.Dict
-function Dict{K,V}(kv::Associative{K,V})
-    h = Dict{K,V}()
-        for (k,v) in kv
-            h[k] = v
-        end
-        return h
-end
-export Dict
-end
-
 # enable logging only during debugging
 #using Logging
 #const logger = Logging.configure(filename="protobuf.log", level=DEBUG)
