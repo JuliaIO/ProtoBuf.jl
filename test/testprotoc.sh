d445e930bb27c63668a447f6791db1e79bfbe679
tan <tanmaykm@gmail.com>
2015-03-24 10:35:00 +0530

    fix #42 defer type till nested types are resolved

--- test/testprotoc.sh
+++ test/testprotoc.sh
@@ -1,4 +1,5 @@
 mkdir -p out
 protoc --proto_path=test/proto --julia_out=out test/proto/t1.proto
+protoc --proto_path=test/proto --julia_out=out test/proto/t2.proto
 protoc --proto_path=test/proto --julia_out=out test/proto/plugin.proto
 protoc --proto_path=test/proto --julia_out=out test/proto/a.proto test/proto/b.proto
