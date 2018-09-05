199f2a45217e137987c2de1cdb37a67685b9816b
Keno Fischer <keno@juliacomputing.com>
2018-09-05 14:27:43 -0400

    Try to use the default script on travis

--- test/testprotoc.jl
+++ test/testprotoc.jl
@@ -6,8 +6,11 @@ else
     test_script = joinpath(@__DIR__, "testprotoc.sh")
     protogen_path = joinpath(@__DIR__, "..", "plugin")
     path_env = "$(protogen_path):$(ENV["PATH"])"
+    modified_env = Dict{String, String}()
     is_ci = "CI" in keys(ENV)
-    cov_flags = is_ci ? """ COVERAGE="--code-coverage=user --inline=no" """ : ""
+    if is_ci
+        modified_env["COVERAGE"] = "--code-coverage=user --inline=no"
+    end
     protoc_compilers = []
     for protoc_env in ("PROTOC2", "PROTOC3")
         if protoc_env in keys(ENV)
@@ -20,9 +23,11 @@ else
         println("testing protobuf compiler plugin...")
         is_ci && println("detected CI environment, will enable code coverage...")
         for protoc_compiler in protoc_compilers
-            cmd = `bash -c "PATH=$(path_env) PROTOC=$(protoc_compiler) $(cov_flags) $(test_script)"`
+            modified_env["PROTOC"] = protoc_compiler
+            modified_env["PATH"] = path_env
+            modified_env["JULIA"] = joinpath(Compat.Sys.BINDIR, Base.julia_exename())
             println("testing protoc compiler plugin with ", protoc_compiler)
-            run(cmd)
+            run(setenv(`$test_script`, modified_env))
         end
     end
 end
