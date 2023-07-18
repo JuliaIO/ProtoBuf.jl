f71d0168a2544bb9d9b1dcc9e7a7a3eb04bca31d
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-18 09:27:24 +0100

    Allowed each key-value pair to be quite long
    Also fixed a typo in a comment

--- src/codec/encoded_size.jl
+++ src/codec/encoded_size.jl
@@ -19,7 +19,7 @@ _encoded_size(x::T, ::Type{Val{:fixed}}) where {T<:Union{Int32,UInt32,Int64,UInt
 
 # For Length-Delimited fields we don't include the encoded number of bytes
 # unless we also provide the field number in which case we encode both the
-# tag and lenght
+# tag and length
 _with_size(n::Int) = (n + _encoded_size(n))
 _encoded_size(x::String) = sizeof(x)
 
@@ -29,19 +29,33 @@ _encoded_size(xs::AbstractVector{T}) where {T<:Union{UInt8,Bool,Float64,Float32}
 _encoded_size(xs::AbstractVector{T}) where {T<:Union{String,AbstractVector{UInt8}}} = sum(x->_with_size(_encoded_size(x)), xs, init=0)
 _encoded_size(xs::AbstractVector{T}, ::Type{Val{:fixed}}) where {T<:Union{Int32,UInt32,Int64,UInt64}} = sizeof(xs)
 
-# Dicts add dummy tags to both keys and values
-_encoded_size(d::AbstractDict) = mapreduce(x->_encoded_size(x.first, 1) + _encoded_size(x.second, 2) + 2, +, d, init=0)
+# Dicts add dummy tags to both keys and values and to each pair
+function _encoded_size(d::AbstractDict)
+    mapreduce(x->begin
+        total_size = _encoded_size(x.first, 1) + _encoded_size(x.second, 2)
+        return 1 + _varint_size(total_size) + total_size
+    end, +, d, init=0)
+end
 
 for T in (:(:fixed), :(:zigzag))
-    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{$(T),Nothing}}}) = mapreduce(x->_encoded_size(x.first, 1, Val{$(T)}) + _encoded_size(x.second, 2) + 2, +, d, init=0)
-    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{Nothing,$(T)}}}) = mapreduce(x->_encoded_size(x.first, 1) + _encoded_size(x.second, 2, Val{$(T)}) + 2, +, d, init=0)
+    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{$(T),Nothing}}}) = mapreduce(x->begin
+        total_size = _encoded_size(x.first, 1, Val{$(T)}) + _encoded_size(x.second, 2)
+        return 1 + _varint_size(total_size) + total_size
+    end, +, d, init=0)
+    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{Nothing,$(T)}}}) = mapreduce(x->begin
+        total_size = _encoded_size(x.first, 1) + _encoded_size(x.second, 2, Val{$(T)})
+        return 1 + _varint_size(total_size) + total_size
+    end, +, d, init=0)
 
     @eval _encoded_size(xs::Union{AbstractDict,AbstractVector}, i::Int, ::Type{Val{$(T)}}) = _encoded_size(i << 3) + _with_size(_encoded_size(xs, Val{$(T)}))
     @eval _encoded_size(xs::Union{Int32,Int64,UInt64,UInt32},   i::Int, ::Type{Val{$(T)}}) = _encoded_size(i << 3) + _encoded_size(xs, Val{$(T)})
 end
 
 for T in (:(:fixed), :(:zigzag)), S in (:(:fixed), :(:zigzag))
-    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{$(T),$(S)}}}) = mapreduce(x->_encoded_size(x.first, 1, Val{$(S)}) + _encoded_size(x.second, 2, Val{$(S)}) + 2, +, d, init=0)
+    @eval _encoded_size(d::AbstractDict, ::Type{Val{Tuple{$(T),$(S)}}}) = mapreduce(x->begin
+        total_size = _encoded_size(x.first, 1, Val{$(T)}) + _encoded_size(x.second, 2, Val{$(S)})
+        return 1 + _varint_size(total_size) + total_size
+    end, +, d, init=0)
 end
 
 # These methods handle fields that refer to messages/groups
